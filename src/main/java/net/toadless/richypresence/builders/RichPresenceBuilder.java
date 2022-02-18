b7ae83cf3b04dc766a7efa2733f3b2bb5dfe4226
toadless <thomasmcdonald2720@gmail.com>
2022-02-18 19:25:23 +0000

    create richypresence builder

---
+++ src/main/java/net/toadless/richypresence/builders/RichPresenceBuilder.java
@@ -0,0 +1,79 @@
+package net.toadless.richypresence.builders;
+
+import net.toadless.discordrpc.DiscordRichPresence;
+import net.toadless.richypresence.Layout;
+
+import java.util.Objects;
+
+public class RichPresenceBuilder
+{
+    private String details;
+    private String state;
+    private String largeImageKey;
+    private String smallImageKey;
+
+    public RichPresenceBuilder()
+    {
+
+    }
+
+    public DiscordRichPresence build()
+    {
+        String details = getDetails();
+        String state = getState();
+        String largeImageKey = getLargeImageKey();
+        String smallImageKey = getSmallImageKey();
+
+        DiscordRichPresence presence = new DiscordRichPresence();
+        presence.details = Objects.equals(details, Layout.DETAILS.getPlaceholder()) ? null : details;
+        presence.state = Objects.equals(state, Layout.STATE.getPlaceholder()) ? null : state;
+        presence.largeImageKey = Objects.equals(largeImageKey, Layout.LARGE_IMAGE_KEY.getPlaceholder()) ? null : largeImageKey;
+        presence.smallImageKey = Objects.equals(smallImageKey, Layout.SMALL_IMAGE_KEY.getPlaceholder()) ? null : smallImageKey;
+
+        return presence;
+    }
+
+    public String getDetails()
+    {
+        return details;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public String getLargeImageKey()
+    {
+        return largeImageKey;
+    }
+
+    public String getSmallImageKey()
+    {
+        return smallImageKey;
+    }
+
+    public RichPresenceBuilder setDetails(String details)
+    {
+        this.details = details;
+        return this;
+    }
+
+    public RichPresenceBuilder setState(String state)
+    {
+        this.state = state;
+        return this;
+    }
+
+    public RichPresenceBuilder setLargeImageKey(String largeImageKey)
+    {
+        this.largeImageKey = largeImageKey;
+        return this;
+    }
+
+    public RichPresenceBuilder setSmallImageKey(String smallImageKey)
+    {
+        this.smallImageKey = smallImageKey;
+        return this;
+    }
+}
